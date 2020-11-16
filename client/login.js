2c2db049c25268454eb6a25b376bbbf3cb3aed45
Pari Yogesh <pariyogesh@1x-nat-vl930-172-30-66-124.wireless.umass.edu>
2020-11-16 21:27:19 +0000

    adding some login/register functionality

---
+++ client/login.js
@@ -0,0 +1,5 @@
+window.addEventListener('load',()=>{
+    document.getElementById('submit').addEventListener('click',()=>{
+        fetch('/login',)
+    });
+});
--- server/server.js
+++ server/server.js
@@ -6,9 +6,57 @@ import { parse } from 'url';
 import { join } from 'path';
 import { readFileSync, existsSync } from 'fs';
 
-const app = express();
-app.use(express.json()); // lets you handle JSON input
+import dotenv from 'dotenv';
+dotenv.config()
+
+import expressSession from 'express-session';
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
+
 const port = process.env.PORT || 8080;
+const app = express();
+
+const session = {
+    secret : process.env.SECRET || 'SECRET', // set this encryption key in Heroku config (never in GitHub)!
+    resave : false,
+    saveUninitialized: false
+};
+// Passport configuration
+const strategy = new LocalStrategy(
+    async (username, password, done) => {
+	if (!findUser(username)) {
+	    // no such user
+	    return done(null, false, { 'message' : 'Wrong username' });
+	}
+	if (!validatePassword(username, password)) {
+	    // invalid password
+	    // should disable logins after N messages
+	    // delay return to rate-limit brute-force attacks
+	    await new Promise((r) => setTimeout(r, 2000)); // two second delay
+	    return done(null, false, { 'message' : 'Wrong password' });
+	}
+	// success!
+	// should create a user object here, associated with a unique identifier
+	return done(null, username);
+    });
+
+// App configuration
+app.use(expressSession(session));
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Convert user object to a unique identifier.
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+// Convert a unique identifier to a user object.
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+app.use(express.json()); // allow JSON inputs
+app.use(express.urlencoded({'extended' : true}));
+
 
 app.get('/readReviews', (req, res) => {
     if ('user' in req.query) {
