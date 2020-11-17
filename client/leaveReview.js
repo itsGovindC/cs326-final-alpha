d8e079a141bd9083a3f3bc5aa7a5bd6496948fbc
Govind <govindchandak06@gmail.com>
2020-11-17 02:51:40 -0500

    login patch for pari's code

--- client/leaveReview.js
+++ client/leaveReview.js
@@ -1,11 +1,9 @@
 window.addEventListener('load', async function() {
     document.getElementById('submit').addEventListener('click', () => {
         const sent_data = {};
-        sent_data['name'] = 'pari'; //will redefine once authentication is done 
         sent_data['dining'] = document.getElementById('Dining-Hall').value;
         sent_data['dish'] = document.getElementById('Dish').value;
         sent_data['review'] = document.getElementById('Review').value;
-        console.log(sent_data);
         fetch('/createReview', {
             method: 'POST',
             body: JSON.stringify(sent_data)
